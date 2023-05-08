19bdfcb7ef3aa2b3a9260cede89bc6f493ff300e
bezgolovyponch <maxieventprag@gmail.com>
2023-05-08 20:52:13 +0200

    initial

---
+++ src/logged_out/components/home/FeatureSection.js
@@ -0,0 +1,141 @@
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+// import CodeIcon from "@mui/icons-material/Code";
+// import BuildIcon from "@mui/icons-material/Build";
+// import ComputerIcon from "@mui/icons-material/Computer";
+// import BarChartIcon from "@mui/icons-material/BarChart";
+// import HeadsetMicIcon from "@mui/icons-material/HeadsetMic";
+// import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+// import CloudIcon from "@mui/icons-material/Cloud";
+// import MeassageIcon from "@mui/icons-material/Message";
+// import CancelIcon from "@mui/icons-material/Cancel";
+import calculateSpacing from "./calculateSpacing";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { withTheme } from "@mui/styles";
+import FeatureCard from "./FeatureCard";
+import useWidth from "../../../shared/functions/useWidth";
+import HandshakeIcon from "@mui/icons-material/Handshake";
+import ExploreIcon from "@mui/icons-material/Explore";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+
+const iconSize = 80;
+
+const features = [
+  {
+    color: "#121F32",
+
+    headline: "Yacht Sales",
+    font: "Bodoni",
+    text: " Bespoke yacht sales services, catering to your specific needs and preferences. We specialize in high-end yachts and work with you to find the perfect vessel for your lifestyle and taste.",
+    icon: <HandshakeIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "0",
+    smDelay: "0",
+  },
+  {
+    color: "#121F32",
+    headline: "Yacht Management",
+    font: "Bodoni",
+    text: "Maintenance, crew management, and financial services. We offer personalized attention and ensure that your yacht is always maintained to the highest standards.",
+    icon: <ExploreIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "200",
+    smDelay: "200",
+  },
+  {
+    color: "#121F32",
+    headline: "Charter Management",
+    font: "Bodoni",
+    text: " We handle all aspects of marketing, booking, and managing your yacht's charter. We provide top-notch service and attention to detail, ensuring your guests have an unforgettable experience.",
+    icon: <EventNoteIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "400",
+    smDelay: "0",
+  },
+  /*   {
+    color: "#d50000",
+    headline: "Feature 4",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+    icon: <ComputerIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "0",
+    smDelay: "200",
+  },
+  {
+    color: "#DD2C00",
+    headline: "Feature 5",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+    icon: <BarChartIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "200",
+    smDelay: "0",
+  },
+  {
+    color: "#64DD17",
+    headline: "Feature 6",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+    icon: <HeadsetMicIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "400",
+    smDelay: "200",
+  },
+  {
+    color: "#304FFE",
+    headline: "Feature 7",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+    icon: <CloudIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "0",
+    smDelay: "0",
+  },
+  {
+    color: "#C51162",
+    headline: "Feature 8",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+    icon: <CodeIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "200",
+    smDelay: "200",
+  },
+  {
+    color: "#00B8D4",
+    headline: "Feature 9",
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+    icon: <CancelIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "400",
+    smDelay: "0",
+  }, */
+];
+
+function FeatureSection(props) {
+  const { theme } = props;
+  const width = useWidth();
+  const isWidthUpMd = useMediaQuery(theme.breakpoints.up("md"));
+
+  return (
+    <div style={{ backgroundColor: "#FFFFFF" }}>
+      <div className="container-fluid">
+        <Typography variant="h3" align="center" className="lg-mg-bottom" style={{ fontFamily: 'Bodoni '}}>
+          Specializations
+        </Typography>
+        <div className="container-fluid">
+          <Grid container spacing={calculateSpacing(width, theme)}>
+            {features.map((element) => (
+              <Grid
+                item
+                xs={12}
+                md={4}
+                data-aos="zoom-in-up"
+                data-aos-delay={isWidthUpMd ? element.mdDelay : element.smDelay}
+                key={element.headline}
+              >
+                <FeatureCard
+                  Icon={element.icon}
+                  color={element.color}
+                  headline={element.headline}
+                  text={element.text}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+FeatureSection.propTypes = {};
+
+export default withTheme(FeatureSection);
---
+++ src/logged_out/components/home/HeadSection.js
@@ -0,0 +1,172 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import { Grid, Typography, Button, Box } from "@mui/material";
+import withStyles from "@mui/styles/withStyles";
+import WaveBorder from "../../../shared/components/WaveBorder";
+import ZoomImage from "../../../shared/components/ZoomImage";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+const styles = (theme) => ({
+  extraLargeButtonLabel: {
+    fontSize: theme.typography.body1.fontSize,
+    [theme.breakpoints.up("sm")]: {
+      fontSize: theme.typography.h6.fontSize,
+    },
+  },
+  extraLargeButton: {
+    paddingTop: theme.spacing(1.5),
+    paddingBottom: theme.spacing(1.5),
+    [theme.breakpoints.up("xs")]: {
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
+    },
+    [theme.breakpoints.up("lg")]: {
+      paddingTop: theme.spacing(2),
+      paddingBottom: theme.spacing(2),
+    },
+    [theme.breakpoints.down("sm")]: {
+      width: "100%",
+    },
+  },
+  card: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("xs")]: {
+      paddingTop: theme.spacing(3),
+      paddingBottom: theme.spacing(3),
+    },
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(5),
+      paddingBottom: theme.spacing(5),
+      paddingLeft: theme.spacing(4),
+      paddingRight: theme.spacing(4),
+    },
+    [theme.breakpoints.up("lg")]: {
+      paddingTop: theme.spacing(6),
+      paddingBottom: theme.spacing(6),
+      paddingLeft: theme.spacing(6),
+      paddingRight: theme.spacing(6),
+    },
+    width: "100%",
+  },
+  wrapper: {
+    position: "relative",
+    backgroundColor: theme.palette.secondary.main,
+    paddingBottom: theme.spacing(2),
+    [theme.breakpoints.up("lg")]: {
+      paddingTop: theme.spacing(12),
+    },
+    [theme.breakpoints.down("sm")]: {
+      paddingTop: 0,
+      "!important": true,
+    },
+  },
+  image: {
+    maxWidth: "100%",
+    verticalAlign: "middle",
+  },
+  container: {
+    marginTop: theme.spacing(6),
+    marginBottom: theme.spacing(12),
+    [theme.breakpoints.down("lg")]: {
+      marginBottom: theme.spacing(9),
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: theme.spacing(3),
+    },
+  },
+  containerFix: {
+    maxWidth: "none !important",
+  },
+  waveBorder: {
+    paddingTop: theme.spacing(4),
+  },
+});
+
+function HeadSection(props) {
+  const { classes, theme } = props;
+  const isWidthUpLg = useMediaQuery(theme.breakpoints.up("lg"));
+
+  return (
+    <Fragment>
+      <div className={classNames(classes.wrapper)}>
+        <div className={classNames("container-fluid", classes.container)}>
+          <div display="flex" justifyContent="center" className="row">
+            <div
+              className={classes.card}
+              data-aos-delay="200"
+              data-aos="zoom-in"
+            >
+              <div className={classNames(classes.containerFix, "container")}>
+                <Box display={"flex"} flexDirection={isWidthUpLg ? "row" : "column"}>
+                  <Grid item xs={12} md={5} order={{ xs: 2, md: 1 }}>
+                    <Box
+                      display="flex"
+                      flexDirection="column"
+                      justifyContent="center"
+                      height="100%"
+                    >
+                      <Box mb={2} textAlign={isWidthUpLg ? "left" : "center"}>
+                        <Typography  style={{ fontFamily: 'Bodoni ', color: "white" }} variant={isWidthUpLg ? "h3" : "h4"}>
+                          LIAISON AND BROKERAGE
+                        </Typography>
+                      </Box>
+                      <div>
+                        <Box mb={3} textAlign={isWidthUpLg ? "left" : "center"}>
+                          <Typography
+                          style={{ fontFamily: 'Bodoni ', color: "white" }} 
+                            variant={isWidthUpLg ? "h5" : "body1"}
+                            color="textSecondary"
+                          >
+                            Experience the effortless luxury of buying or
+                            selling your dream yacht with the guidance of our
+                            multilingual team of expert brokers
+                          </Typography>
+                        </Box>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          className={classes.extraLargeButton}
+                          classes={{ label: classes.extraLargeButtonLabel }}
+                          onClick={() => {
+                            const footer = document.querySelector("#footer");
+                            footer.scrollIntoView({ behavior: "smooth" });
+                          }}
+                        >
+                          Book consultation now
+                        </Button>
+                      </div>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12} md={7} order={{ xs: 1, md: 2 }}>
+                    <Box display="flex" justifyContent={isWidthUpLg ? "flex-end" : "center"}>
+                      <ZoomImage
+                        src={`${process.env.PUBLIC_URL}/images/logged_out/yacht.png`}
+                        className={classes.image}
+                        alt="yacht"
+                      />
+                    </Box>
+                  </Grid>
+                </Box>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <WaveBorder
+        upperColor={theme.palette.secondary.main}
+        lowerColor="#FFFFFF"
+        className={classes.waveBorder}
+        animationNegativeDelay={2}
+      />
+    </Fragment>
+  );
+}
+
+HeadSection.propTypes = {
+  classes: PropTypes.object,
+  theme: PropTypes.object,
+};
+
+export default withStyles(styles, { withTheme: true })(HeadSection);
